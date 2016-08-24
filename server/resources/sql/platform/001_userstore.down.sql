3605cb5313fcce89a71d74d1c63c573ba0be6a86
apremalal <anuruddhapremalal@gmail.com>
2016-08-24 19:53:41 +0530

    removing database migration

--- server/resources/sql/platform/001_userstore.down.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE `vs_tenants`;
-DROP TABLE `vs_users`;
-DROP TABLE `vs_permissions`;
-DROP TABLE `vs_user_permissions`;
--- server/resources/sql/platform/003_locations.down.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE wf_zones;
-DROP TABLE wf_venues;
-DROP TABLE wf_zone_venues;
-DROP TABLE wf_aps;
-DROP TABLE wf_venue_aps;
-DROP TABLE wf_ssids;
