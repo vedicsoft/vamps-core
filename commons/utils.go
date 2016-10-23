25c6a9e12dc0f95e84eedc51f2d8b70d33d3a8b6
apremalal <anuruddhapremalal@gmail.com>
2016-10-23 20:39:57 +0530

    adding getTenantIDFromDomain util method

--- commons/utils.go
+++ commons/utils.go
@@ -1,11 +1,30 @@
 package commons
 
 import (
+	"errors"
 	"net/http"
 	"strconv"
 )
 
+const GET_TENANT_ID_FROM_DOMAIN string = "SELECT tenantid from vs_tenants WHERE domain = ?"
+
 func GetTenantId(r *http.Request) (int, error) {
 	tenantId, err := strconv.Atoi(r.Header.Get("tenantid"))
 	return tenantId, err
+}
+
+func GetTenantIDFromDomain(tenantDomain string) (int, error) {
+	dbMap := GetDBConnection(PLATFORM_DB)
+	tenantID, err := dbMap.SelectNullInt(GET_TENANT_ID_FROM_DOMAIN, tenantDomain)
+	if err != nil {
+		errMsg := "error occurred while getting tenant id for domain: " + tenantDomain + "  stack trace:" + err.Error()
+		return 0, errors.New(errMsg)
+	} else {
+		if tenantID.Valid {
+			return int(tenantID.Int64), nil
+		} else {
+			return 0, errors.New("error occurred while getting tenant id for domain :" + tenantDomain)
+		}
+
+	}
 }
