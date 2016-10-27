0a9301edd07f5559fb3995fd6665b755466f1155
apremalal <anuruddhapremalal@gmail.com>
2016-10-27 14:45:23 +0530

    added mac validation and normalization

--- commons/utils.go
+++ commons/utils.go
@@ -3,10 +3,13 @@ package commons
 import (
 	"errors"
 	"net/http"
+	"regexp"
 	"strconv"
+	"strings"
 )
 
 const GET_TENANT_ID_FROM_DOMAIN string = "SELECT tenantid from vs_tenants WHERE domain = ?"
+const REGEX_MAC string = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$"
 
 func GetTenantId(r *http.Request) (int, error) {
 	tenantId, err := strconv.Atoi(r.Header.Get("tenantid"))
@@ -27,4 +30,22 @@ func GetTenantIDFromDomain(tenantDomain string) (int, error) {
 		}
 
 	}
+}
+
+// format XX-XX-XX-XX-XX-XX
+func NormalizeMAC(mac string) (string, error) {
+	if len(mac) > 0 {
+		if len(mac) == 12 {
+			return strings.ToUpper(mac[:2] + "-" + mac[2:4] + "-" + mac[4:6] + "-" + mac[6:8] + "-" +
+				mac[8:10] + "-" + mac[10:12]), nil
+		} else {
+			return strings.ToUpper(strings.Replace(mac, ":", "-", -1)), nil
+		}
+	}
+	return "", errors.New("empty MAC address")
+}
+
+func IsValidMAC(mac string) (bool, error) {
+	match, err := regexp.MatchString("^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$", "aa-11-bb-ee-cc-23")
+	return match, err
 }
