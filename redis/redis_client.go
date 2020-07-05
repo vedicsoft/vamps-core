3653fa3b796d39d15ebdc851c1f2feb22109343d
desilvaNSP <desilva.nsp@gmail.com>
2020-07-06 00:05:18 +0530

    Refactored source code based on latest version of github.com/go-redis/redis

--- redis/redis_client.go
+++ redis/redis_client.go
@@ -1,6 +1,7 @@
 package redis
 
 import (
+	"context"
 	"time"
 
 	"github.com/go-redis/redis"
@@ -8,7 +9,9 @@ import (
 )
 
 var rg *Connection
+var ctx = context.Background()
 
+// Connection ....
 type Connection struct {
 	clusterClient  *redis.ClusterClient
 	client         *redis.Client
@@ -28,24 +31,26 @@ func init() {
 	})
 }
 
+// SetValue ...
 func SetValue(key string, value string, expiration int64) error {
 	if rg.ClusterEnabled {
-		return rg.clusterClient.Set(key, value, time.Duration(expiration)*time.Second).Err()
+		return rg.clusterClient.Set(ctx, key, value, time.Duration(expiration)*time.Second).Err()
 	} else {
-		return rg.client.Set(key, value, time.Duration(expiration)*time.Second).Err()
+		return rg.client.Set(ctx, key, value, time.Duration(expiration)*time.Second).Err()
 	}
 }
 
+// GetValue ...
 func GetValue(key string) (string, error) {
 	if rg.ClusterEnabled {
-		val, err := rg.clusterClient.Get(key).Result()
+		val, err := rg.clusterClient.Get(ctx, key).Result()
 		if redis.Nil == err {
 			return "", nil
 		} else {
 			return val, err
 		}
 	} else {
-		val, err := rg.client.Get(key).Result()
+		val, err := rg.client.Get(ctx, key).Result()
 		if redis.Nil == err {
 			return "", nil
 		} else {
